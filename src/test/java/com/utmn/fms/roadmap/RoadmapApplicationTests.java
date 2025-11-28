eccbfe652385b957181aa9c2509a0f6d637667dc
Антон Шанаурин <ant@MacBook-Air-Anton.local>
2025-11-28 19:10:59 +0500

    Initial commit

---
+++ src/test/java/com/utmn/fms/roadmap/RoadmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utmn.fms.roadmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
